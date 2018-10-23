f42e92ffc0d9215fd6f9e82b2791e2b708d163cb
Bryant A <bryantaparicio@hotmail.com>
2018-10-23 11:27:21 -0700

    Added Modules
    Created blank modules for Labratory 2.

---
+++ CSE401_Lab_Project.srcs/sources_1/new/reg.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/23/2018 11:23:27 AM
+// Design Name: 
+// Module Name: REG
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module REG(
+
+    );
+endmodule
