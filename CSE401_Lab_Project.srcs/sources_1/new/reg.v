518b4367363a8b002c2f5bd36b78f3f6ff894e51
riley <sherwood240@gmail.com>
2018-10-30 22:43:53 -0700

    reg

--- CSE401_Lab_Project.srcs/sources_1/new/reg.v
+++ CSE401_Lab_Project.srcs/sources_1/new/reg.v
@@ -12,6 +12,45 @@
 
 
 module REG(
-
-    );
+input [4:0] rs,
+input [4:0] rt,
+input [4:0] rd,
+input [31:0] writedata,
+input regwrite,
+output reg [31:0] A,
+output reg [31:0] B
+);
 endmodule
+
+//Reg declaration
+
+reg [31:0] REG [0:31]; 
+	
+integer i;
+	
+initial 
+begin
+	    
+	    A <= 0;
+	    B <= 0;
+		  
+		  
+		  for (i = 0; i < 32; i = i + 1)
+			    REG[i] <= 0;
+		
+     
+     $display("From Register Memory:");
+		 for (i = 0; i < 9; i = i + 1)
+			 $display("\tREG[%0d] = %0d",i,REG[i]);
+		
+		
+		  $display("\t...");
+		  $display("\tREG[%0d] = %0d", 31, REG[31]);
+end
+always @ *
+A <- rs [4:0];
+B <= rt [4:0];
+
+if (rd != 0 && regwrite)
+
+rd = reg [31:0] B;
